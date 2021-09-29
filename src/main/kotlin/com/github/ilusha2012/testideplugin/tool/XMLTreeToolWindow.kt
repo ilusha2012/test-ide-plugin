398921936f3db160aed73c0bde91aae670053b5c
Ilya Shaydullin <shaydullin@icerockdev.com>
2021-09-29 15:21:33 +0700

    Get xml content from file and set it to JTree component

--- src/main/kotlin/com/github/ilusha2012/testideplugin/tool/XMLTreeToolWindow.kt
+++ src/main/kotlin/com/github/ilusha2012/testideplugin/tool/XMLTreeToolWindow.kt
@@ -1,12 +1,15 @@
 package com.github.ilusha2012.testideplugin.tool
 
 import com.intellij.openapi.wm.ToolWindow
-import java.io.File
 import javax.swing.JPanel
 import javax.swing.JTree
 import javax.swing.tree.DefaultMutableTreeNode
-import javax.xml.parsers.SAXParserFactory
-import org.xml.sax.helpers.DefaultHandler
+import javax.swing.tree.DefaultTreeModel
+import javax.xml.parsers.DocumentBuilderFactory
+import org.bouncycastle.asn1.x500.style.RFC4519Style.title
+import org.w3c.dom.Document
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 
 
 class XMLTreeToolWindow(toolWindow: ToolWindow) {
@@ -14,16 +17,30 @@ class XMLTreeToolWindow(toolWindow: ToolWindow) {
     var content: JPanel? = null
 
     init {
-//        val base = DefaultMutableTreeNode("XML Viewer")
-//
-//        val fact = SAXParserFactory.newInstance()
-//        val parser = fact.newSAXParser()
-//        val defaultHandler = DefaultHandler()
-//        parser.parse(File("/Users/mac2/Downloads/wh-java-dev-test/sample-file.xml"), defaultHandler)
-//
-//        defaultHandler.startDocument()
-//
-//
-        xmlTree!!
+
+        val factory = DocumentBuilderFactory.newInstance()
+        val builder = factory.newDocumentBuilder()
+        val doc: Document = builder.parse("/Users/mac2/Downloads/wh-java-dev-test/sample-file.xml")
+        val root: Node = doc.documentElement as Node
+
+        val dtModel = DefaultTreeModel(builtTreeNode(root))
+        xmlTree!!.model = dtModel
+    }
+
+    private fun builtTreeNode(root: Node): DefaultMutableTreeNode {
+        val title = if (root.attributes.getNamedItem("title") != null) {
+            root.attributes.getNamedItem("title").nodeValue
+        } else {
+            root.textContent
+        }
+        val dmtNode = DefaultMutableTreeNode(title)
+        val nodeList: NodeList = root.childNodes
+        for (index in 0 until nodeList.length) {
+            val tempNode: Node = nodeList.item(index)
+            if (tempNode.nodeType == Node.ELEMENT_NODE && tempNode.hasChildNodes()) {
+                dmtNode.add(builtTreeNode(tempNode))
+            }
+        }
+        return dmtNode
     }
 }
